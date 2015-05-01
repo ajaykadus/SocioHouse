95dda95e5c909a713ff6ddffb63fbfa2ab55aec1
Ajay Kadus <ajay.kadus@move.com>
2015-04-30 21:11:37 -0700

    making slider for bidding

--- client/views/bidding/bidding.js
+++ client/views/bidding/bidding.js
@@ -1,5 +1,10 @@
 /**
  * Created by akadus on 2/21/15.
  */
-Template.myBidding.rendered = function(){
-    };
+Template.myBidding.rendered = function () {
+};
+Template.myBidding.events({
+    "input #js-bid-range": function (event, template) {
+        $(template.find("output#range")).html($(event.currentTarget).val());
+    }
+});
